8c37c48b7158f0c27ee2be59016f162b1c0b2f7d
Venkatesh <venkatesh@brio.co.in>
2020-10-21 17:45:42 +0530

    static errors3

--- bsf/urls.py
+++ bsf/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bsf_app.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
